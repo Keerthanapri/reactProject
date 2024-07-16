1ab1be1d8cafdefc891b47d05b448f5c30c64140
Keerthanapriya D R <keerthanapriyaramamoorthy@gmail.com>
2024-07-16 23:19:49 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import AddNumber from './components/AddNumber';
+import "./App.css"
 
 function App() {
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+          <AddNumber />
+       
     </div>
+       
+        
+      
   );
 }
-
-export default App;
